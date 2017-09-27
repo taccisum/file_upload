86c43288e75ae1d2a9e2b8495efc74ccffaa0514
tac <514162920@qq.com>
2017-09-27 18:48:40 +0800

    import project

---
+++ src/main/java/cn/tac/template/fileupload/FileUploadApplication.java
@@ -0,0 +1,19 @@
+package cn.tac.template.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tac
+ * @since 27/09/2017
+ */
+
+
+@SpringBootApplication
+public class FileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadApplication.class, args);
+    }
+
+}
